b3326c9c86507e758b41ef8952a5636524bd5ac3
AC Abasolo <ac@devwerkz.com>
2025-10-22 21:44:11 +0800

    create email draft

--- src/analyzer.ts
+++ src/analyzer.ts
@@ -5,10 +5,8 @@ import {Agency} from "./interfaces/agency.interface";
 import {GoogleSheetWriter} from "./googleSheetWriter";
 export class Analyzer {
     public async run() : Promise<void> {
-        // const prompt = `Analyze the following JSON data and provide a summary: ${JSON.stringify(myData)}`;
-        // const prompt = `Create a visual representation (like a tree or chart) of this JSON: ${JSON.stringify(myData)}`;
-        const prompt = `Please Analyze the following JSON data and summarize the reviews and the summarize the profile of each item: ${JSON.stringify(myData)}`;
-        // const prompt = `Here is the data for 5 different agencies: ${JSON.stringify(myData[0])}`;
+        const prompt = `Analyze the following JSON data and provide a summary: ${JSON.stringify(myData)}`;
+        // const prompt = `Please Analyze the following JSON data and summarize the reviews and the summarize the profile of each item: ${JSON.stringify(myData)}`;
         const response = await ollama.generate({
             model: 'gemma3', // Replace with your desired model
             prompt: prompt,
@@ -19,7 +17,6 @@ export class Analyzer {
     public async writeToSheet() : Promise<void> {
         const googleSheetWriter = new GoogleSheetWriter('1FFjaZnZDX90_QrFyXRW13ayZU8USHUCmzsFszbU8rp8');
 
-        // const prompt = `Please Analyze the following JSON data and then extract the title, summarized reviews and summarized profile of each item: ${JSON.stringify(myData)}`;
         const prompt = `Please extract the "title", summarize the "reviews", create a shorter summary of the "profile" from the following JSON data: ${JSON.stringify(myData)}`;
 
         const schema = {
@@ -35,21 +32,37 @@ export class Analyzer {
         };
 
         const response = await ollama.generate({
-            model: 'gemma3', // Replace with your desired model
+            model: 'gemma3',
             prompt: prompt,
             format: schema,
-            // format: 'json',
         });
-        // console.log('Ollama response:', response.response);
-        // console.log('Ollama response:', JSON.parse(response.response));
+
         console.log('writing to Sheet');
         await googleSheetWriter.write(JSON.parse(response.response));
         console.log('done');
+    }
+
+    public async contactLeads() : Promise<void> {
+        const prompt = "Your name is \"Jymyl Bandong\" and you are the CEO of a web development company named \"DevDaddies\".\n" +
+            "\n" +
+            "Your contact phone is \"1234567890\" and your email is \"jym@devdaddies.com\".\n" +
+            "\n" +
+            "Do not show any notes and customization guidelines." +
+            "\n" +
+            "Analyze the following JSON data that contains a provider and compose an email inviting them for a collaboration project: " + JSON.stringify(myData[0]);
+
+        // const prompt = `Please Analyze the following JSON data and summarize the reviews and the summarize the profile of each item: ${JSON.stringify(myData)}`;
+        const response = await ollama.generate({
+            model: 'gemma3', // Replace with your desired model
+            prompt: prompt,
+        });
+        console.log('Ollama response:', response.response);
     }
 }
 
 (async () => {
     const analyzer = new Analyzer();
     // await analyzer.run();
-    await analyzer.writeToSheet();
+    // await analyzer.writeToSheet();
+    await analyzer.contactLeads();
 })();
